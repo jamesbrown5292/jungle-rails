784c6e31f585e46f9e4de2710895a4b4fbb55937
James Brown <jamesbrown5292@gmail.com>
2020-10-15 23:28:28 +0000

    implement site stats and style button

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"],
+  if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+  
   def show
+    @total_categories= Category.distinct.count
+    @total_inventory = Product.sum(:quantity)
   end
+
 end
