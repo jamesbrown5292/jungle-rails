94ced8846d7edbddf27ae97d7e14804c29070f51
James Brown <jamesbrown5292@gmail.com>
2020-10-19 23:14:33 +0000

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :name, :email, :password, :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 
+  describe 'Validations' do
+    it 'should fail when instantiated with no password' do 
+      testUser = User.new(:name => "testy", :email => "testy@test.com", :password => "admin", :password_confirmation => nil)
+      testUser.save
+      puts testUser.errors.full_messages
+      expect(testUser).to be_falsey
+    end
+  end
 end
