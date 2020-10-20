4b2d9dadb4fb7cc36c517f11fb723cccf0ddc035
James Brown <jamesbrown5292@gmail.com>
2020-10-20 22:03:54 +0000

    implement product detail spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,66 @@
 # require 'rails_helper'
 
-# RSpec.feature "ProductDetails", type: :feature do
-#   pending "add some scenarios (or delete) #{__FILE__}"
+# RSpec.feature "visitor naviagates to product page from home page", type: :feature, js: true do
+#     # SETUP
+#   before :each do
+#     @category = Category.create! name: 'Apparel'
+
+#     10.times do |n|
+#       @category.products.create!(
+#         name:  Faker::Hipster.sentence(3),
+#         description: Faker::Hipster.paragraph(4),
+#         image: open_asset('apparel1.jpg'),
+#         quantity: 10,
+#         price: 64.99
+#       )
+#     end
+#   end 
+
+#   scenario "They click a product and are taken to the product details page"
+
+#     #ACT
+#     visit root_path
+#     click_on 'Details'
+
+#     #DEBUG
+#     save_screenshot
+#   end
 # end
+
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    first('article.product').click_link 'Details'
+    expect(page).to have_css('.products-show')
+    find('img').visible?
+
+
+
+    
+    # DEBUG
+    save_screenshot
+    
+    # VERIFY
+
+
+  end
+end
