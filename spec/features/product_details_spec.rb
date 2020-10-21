823d7018c1e20d03fadb72c98a12e77a229d4798
James Brown <jamesbrown5292@gmail.com>
2020-10-21 15:23:41 +0000

    add feature specs

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,37 +1,37 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
-#   # SETUP
-#   before :each do
-#     @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-#     10.times do |n|
-#       @category.products.create!(
-#         name:  Faker::Hipster.sentence(3),
-#         description: Faker::Hipster.paragraph(4),
-#         image: open_asset('apparel1.jpg'),
-#         quantity: 10,
-#         price: 64.99
-#       )
-#     end
-#   end
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
 
-#   scenario "They see all products" do
-#     # ACT
-#     visit root_path
-#     first('article.product').click_link 'Details'
-#     expect(page).to have_css('.products-show')
-#     find('img').visible?
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    first('article.product').click_link 'Details'
+    expect(page).to have_css('.products-show')
+    find('img').visible?
 
 
 
     
-#     # DEBUG
-#     save_screenshot
+    # DEBUG
+    save_screenshot
     
-#     # VERIFY
+    # VERIFY
 
 
-#   end
-# end
+  end
+end
